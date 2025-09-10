0cc7c8972eb55c7abaa36904b69f65aef4d4562e
Abdelrahman Emara <abdelrahmanemara155@gmail.com>
2025-09-10 12:29:27 +0300

    Using Props in Pizza List

--- src/index.js
+++ src/index.js
@@ -69,20 +69,31 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name="Margherita Pizza"
+        ingredients="Tomato and mozarella"
+        photoName="pizzas/margherita.jpg"
+        price={20}
+      />
+      <Pizza
+        name="Pizza Prosciutto"
+        ingredients="Tomato, mozarella, ham, aragula, and burrata cheese"
+        photoName="pizzas/prosciutto.jpg"
+        price={30}
+      />
     </main>
   );
 }
 
-function Pizza() {
+function Pizza(props) {
   return (
-    <div>
-      <img src="pizzas/margherita.jpg" alt="Margherita Pizza" />
-      <h3>Margherita Pizza</h3>
-      <p>Tomato and mozarella</p>
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price}</span>
+      </div>
     </div>
   );
 }
